ab7761465255c5cf38c2732fba466e71e9c689f7
Mark Albiston <mark.albiston@gmail.com>
2019-12-19 14:54:24 -0700

    display winner, clear list to start new game

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -10,16 +10,17 @@ namespace CardsAgainstMadLibs.Hubs {
         public string EncodedMsg { get; set; }
         public int VoteCount { get; set; } = 0;
         public int UserId { get; set; }
-        public User User { get; set; }
+        public string Username { get; set; }
     }
     public class ChatHub : Hub {
         static List<CardSubmission> Submissions = new List<CardSubmission> ();
-        public async Task SendMessage (string verb, string msg, string str1, string str2, string str3, int userId) {
+        public async Task SendMessage (string verb, string msg, string str1, string str2, string str3, int userId, string Username) {
             string encodedMsg = str1 + " " + verb + " " + str2 + " " + msg + " " + str3;
             CardSubmission newSubmission = new CardSubmission()
             {
                 EncodedMsg = encodedMsg,
-                UserId = userId
+                UserId = userId,
+                Username = Username
             };
             ChatHub.Submissions.Add (newSubmission);
             await Clients.All.SendAsync ("ReceiveMessage", Submissions);
@@ -31,20 +32,41 @@ namespace CardsAgainstMadLibs.Hubs {
 
             // if(CSub.UserId == (int)HttpContext.Session.GetInt32("currentuser"))
             int index = Submissions.IndexOf(Submissions.Where(x => x.UserId == CSub.UserId).FirstOrDefault());
-            System.Console.WriteLine(index);
+            // System.Console.WriteLine(index);
             Submissions[index].VoteCount += 1;
             await Clients.All.SendAsync ("ReceiveMessage", Submissions);
 
         }
 
-        public async Task CalculateWinner()
+        public async Task<string> CalculateWinner()
         {
-            System.Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-            CardSubmission winner = (CardSubmission)Submissions.OrderByDescending(User => User.VoteCount ).Take(1);
-            string winnernametodisplay = winner.User.Username;
-            int winningvotes = winner.VoteCount;
-            System.Console.WriteLine($"{winnernametodisplay} is the winner with {winningvotes} votes!");
-            await Clients.All.SendAsync ("ReceiveMessage", Submissions);
+            // System.Console.WriteLine("*********%%%%%%%%%%%*********%%%%%%%%%%%*********%%%%%%%%%%%");
+            List<CardSubmission> winnercard = Submissions.OrderByDescending(User => User.VoteCount ).Take(1).ToList();
+            // System.Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+            System.Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(winnercard));
+            // System.Console.WriteLine("######################################");
+            string winnernametodisplay = winnercard[0].Username;
+            
+            // List<CardSubmission> listwinner = (List<CardSubmission>)winner;
+            System.Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(winnercard));
+
+
+            // System.Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            // int winningvotes = winner.VoteCount;
+            // System.Console.WriteLine($"{winnernametodisplay} is the winner with {winningvotes} votes!");
+            // // await Clients.All.SendAsync ("ReceiveMessage", Submissions);
+            // System.Console.WriteLine("******************************************************");
+            System.Console.WriteLine($"{winnernametodisplay} is the WINNER!!!!!!!!!!!!!!");
+
+            // System.Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            return await Task.FromResult<string>(winnernametodisplay);
+        }
+
+        public void PlayAgain()
+        {
+            ChatHub.Submissions.Clear();
+            System.Console.WriteLine("*********************");
+            // await Clients.All.SendAsync ("ReceiveMessage", Submissions);
         }
     }
 }
--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -3,142 +3,189 @@
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 // Disable send button until connection is established
-document.getElementById("sendButton").disabled = true;
-let timer;
-let countdown = 30;
-let interval;
-//jb
-let hasvoted = false;
-let gameover = false;
-let timerWindow = document.createElement("div");
-timerWindow.id = "timerWindow";
-timerWindow.style.position = "absolute";
-timerWindow.style.top = "50px";
-timerWindow.style.right = "60px";
-timerWindow.style.color = "red";
-timerWindow.style.fontSize = "xx-large";
-// timerWindow.textContent = 10;
-timerWindow.style.display = "none";
 
-document.body.appendChild(timerWindow);
-connection.on("ReceiveMessage", function (arrayOfMessages = []) {
-    // console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-    // if(interval || timeout){
-    //     console.log("*********************************");
-    //     console.log(timeout);
-    //     if(timer) clearTimeout(timeout);
-    //     console.log("*********************************");
-    //     console.log(interval);
-    //     console.log(countdown);
-    //     if(interval) clearInterval(interval);
-    //     countdown = 30;
-    // }
-    // console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
-    // if(countdown != 30){
-        
-    //     countdown = 30;
-    // } 
-
-    // timer = setTimeout(()=> {
-    //         // console.log("Times up mother fucker");
-    //         var seconds = document.getElementById("timerWindow").textContent;
-    //         if (seconds <=0 ) clearInterval(countdown);
-    //         // clearInterval(countdown);
-    //     }, 10000);
-    //     interval = setInterval(()=> {
-    //             timerWindow.innerHTML = countdown;
-    //             countdown--;
-    //         },1000)
-
-
-    if(seconds != 10){
-        console.log("############################");
-        clearInterval(countdown)
-    }
+$(document).ready(function () {
+    console.log("document has loaded");
+    $('.hide').click(function () {
+        $('.hide-me').hide()
+    });
+    $('.hide').click(function () {
+        $('.show-me').show()
+    });
+    document.getElementById("sendButton").disabled = true;
+    let timer;
+    // let countdown = 30;
+    let timers = {};
+    let interval;
+    //jb
+    let hasvoted = false;
+    let gameover = false;
+    let timerWindow = document.createElement("div");
+    timerWindow.id = "timerWindow";
+    timerWindow.style.position = "absolute";
+    timerWindow.style.top = "50px";
+    timerWindow.style.right = "60px";
+    timerWindow.style.color = "red";
+    timerWindow.style.fontSize = "xx-large";
+    // timerWindow.textContent = 10;
+    timerWindow.style.display = "none";
     timerWindow.style.display = "inline";
-    timerWindow.textContent = 10;
-    var seconds = document.getElementById("timerWindow").textContent;
-    var countdown = setInterval(function(){
-        seconds --;
-        document.getElementById("timerWindow").textContent = seconds;
-        if (seconds <= 0){
-            clearInterval(countdown);
-            connection.invoke("CalculateWinner").catch(function (err) {
-                console.error(err.toString());
-            });
-            gameover = true;
-        } 
-    }, 1000);
+    var resetbutton = document.getElementById("Reset");
+    var voteline = document.getElementById("VoteLine");
+    var winnerline = document.getElementById("winnerline");
+    document.body.appendChild(timerWindow);
+    // var seconds = document.getElementById("timerWindow").textContent;
+    // var countdown;
 
-
-
-   console.log(arrayOfMessages);
-   var list = document.getElementById("messagesList")
-   list.innerHTML = ""; //check out if "this" idea does not work
-   arrayOfMessages.forEach(cardInput => {
-       var li = document.createElement("button"); //change to separate div instead of making a list
-       li.classList.add("btn");
-       li.classList.add("btn-dark");
-       li.classList.add("my-2");
-       li.style.width = "40%";
-       li.style.minHeight = "170px";
-       li.style.marginRight = "3%";
-       li.style.marginRight = "3%";
-       li.style.backgroundColor = "#202020";
-    //    li.classList.add("limitvote");
-       li.textContent = cardInput.encodedMsg + "-------------->  VOTES: " + cardInput.voteCount;
-       li.addEventListener("click", function() {
-           // add to the count of this exact card.
-        //    li.classList.add("disabled");
-            if (!hasvoted)
-            {
-                connection.invoke("Vote", cardInput).catch(function (err) {
-                     console.error(err.toString());
-                 });
-                 hasvoted = true;
+    function ResetTimer() {
+        let seconds = 10;
+        return setInterval(() => {
+            seconds--;
+            console.log(seconds);
+            var timerwindow = $("#timerWindow");
+            console.log("message to us TIMER WINDOW TEXT", timerwindow.text());
+            timerwindow.text(`${seconds}`);
+            if (seconds <= 0) {
+                clearInterval(timers["interval"]);
+                timers["interval"] = null;
+                var winnername =  connection.invoke("CalculateWinner").catch(function (err) {
+                    console.error(err.toString());
+                });
+                console.log(winnername);
+                
+                resetbutton.style.display = "inline";
+                let cardList = document.getElementById("messagesList");
+                cardList.innerHTML = "";
+                winnerline.style.display = "block";
+                voteline.style.display = "none";
+                // let placeholer = "USERNAME";
+                winnername.then(function(result){
+                    winnerline.textContent = ("Congratulations, " + result + " is the winner!");
+                })
+                
+                gameover = true;
             }
-            // document.getElementsByClassName("limitvote").disabled = true;
-       });
-    //    list.appendChild(li);
-       list.insertBefore(li, list.firstChild);
-   });
-   // loop through arrayOfMessages
+        }, 1000);
+    }
 
 
-});
-       document.getElementById("Reset").addEventListener("click", function (){
-           let cardList = document.getElementById("messagesList");
+    connection.on("ReceiveMessage", function (arrayOfMessages = []) {
+        // console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        // if(interval || timeout){
+        //     console.log("*********************************");
+        //     console.log(timeout);
+        //     if(timer) clearTimeout(timeout);
+        //     console.log("*********************************");
+        //     console.log(interval);
+        //     console.log(countdown);
+        //     if(interval) clearInterval(interval);
+        //     countdown = 30;
+        // }
+        // console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        // if(countdown != 30){
+
+        //     countdown = 30;
+        // } 
+
+        // timer = setTimeout(()=> {
+        //         // console.log("Times up mother fucker");
+        //         var seconds = document.getElementById("timerWindow").textContent;
+        //         if (seconds <=0 ) clearInterval(countdown);
+        //         // clearInterval(countdown);
+        //     }, 10000);
+        //     interval = setInterval(()=> {
+        //             timerWindow.innerHTML = countdown;
+        //             countdown--;
+        //         },1000)
+
+
+
+        timerWindow.textContent = 10;
+        if (!timers["interval"]) {
+
+            timers["interval"] = ResetTimer();
+        }
+        else {
+            console.log("############################", timers["interval"]);
+            clearInterval(timers["interval"]);
+            timers["interval"] = null;
+            timers["interval"] = ResetTimer();
+
+        }
+
+        // document.getElementById("timerWindow").addEventListener("oninput")
+
+
+        console.log(arrayOfMessages);
+        var list = document.getElementById("messagesList")
+        list.innerHTML = ""; //check out if "this" idea does not work
+        arrayOfMessages.forEach(cardInput => {
+            var li = document.createElement("button"); //change to separate div instead of making a list
+            li.classList.add("btn");
+            li.classList.add("btn-dark");
+            li.classList.add("my-2");
+            li.style.width = "40%";
+            li.style.minHeight = "170px";
+            li.style.marginRight = "3%";
+            li.style.marginRight = "3%";
+            li.style.backgroundColor = "#202020";
+            //    li.classList.add("limitvote");
+            li.textContent = cardInput.encodedMsg + "-------------->  VOTES: " + cardInput.voteCount;
+            li.addEventListener("click", function () {
+                // add to the count of this exact card.
+                //    li.classList.add("disabled");
+                if (!hasvoted) {
+                    connection.invoke("Vote", cardInput).catch(function (err) {
+                        console.error(err.toString());
+                    });
+                    hasvoted = true;
+                }
+                // document.getElementsByClassName("limitvote").disabled = true;
+            });
+            //    list.appendChild(li);
+            list.insertBefore(li, list.firstChild);
+        });
+        // loop through arrayOfMessages
+
+
+    });
+    document.getElementById("Reset").addEventListener("click", function () {
+        let cardList = document.getElementById("messagesList");
         //    while(arrayOfMessages.firstChild){
         //        arrayOfMessages.pop();
         //    }
         //    while (cardList.firstChild){
         //        cardList.removeChild(cardList.firstChild);
         //    }
-           cardList.innerHTML = "";
-       
-       })
+        //    location.reload(true);
+        connection.invoke("PlayAgain").catch(function (err) {
+            console.error(err.toString());
+        });
+        //    cardList.textContent = "";
+    })
 
 
 
 
 
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
-});
-
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    event.preventDefault();
-    var verb = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    var str1 = document.getElementById("string1").value;
-    var str2 = document.getElementById("string2").value;
-    var str3 = document.getElementById("string3").value;
-    var userId = +document.getElementById("loggeduserId").value;
-    
-    connection.invoke("SendMessage", verb, message, str1, str2, str3, userId).catch(function (err) {
+    connection.start().then(function () {
+        document.getElementById("sendButton").disabled = false;
+    }).catch(function (err) {
         return console.error(err.toString());
     });
-}); 
 
+    document.getElementById("sendButton").addEventListener("click", function (event) {
+        event.preventDefault();
+        var verb = document.getElementById("userInput").value;
+        var message = document.getElementById("messageInput").value;
+        var str1 = document.getElementById("string1").value;
+        var str2 = document.getElementById("string2").value;
+        var str3 = document.getElementById("string3").value;
+        var userId = +document.getElementById("loggeduserId").value;
+        var Username = document.getElementById("loggedUser").value;
+        connection.invoke("SendMessage", verb, message, str1, str2, str3, userId, Username).catch(function (err) {
+            return console.error(err.toString());
+        });
+    });
+
+});
